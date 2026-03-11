e53eaef421d3c2704d6f227d9a3500763e9e8634
Satyam-Kumar <satyam.k.2105@gmail.com>
2026-03-11 15:04:50 +0530

    production API URL config

--- sentinel-ui/src/main.jsx
+++ sentinel-ui/src/main.jsx
@@ -8,7 +8,7 @@ import { create } from "zustand";
 import App from "./App.jsx";
 import "./index.css";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const queryClient = new QueryClient({
   defaultOptions: {
